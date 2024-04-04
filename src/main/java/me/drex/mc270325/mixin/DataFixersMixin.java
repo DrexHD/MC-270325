3f72c5211981f2887f539f026ed68ba8d86a6182
Drex <nicknamedrex@gmail.com>
2024-04-04 11:10:18 +0200

    Improve injection target
    MC-270325 has been marked as Wont Fix, so we need to ensure our
    datafixer is registered at the correct time, even with datafixers added 
    in the future.

--- src/main/java/me/drex/mc270325/mixin/DataFixersMixin.java
+++ src/main/java/me/drex/mc270325/mixin/DataFixersMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BiFunction;
@@ -21,7 +22,23 @@ public abstract class DataFixersMixin {
     private static BiFunction<Integer, Schema, Schema> SAME_NAMESPACED;
 
     // Add fixer for the correct version
-    @Inject(method = "addFixers", at = @At(value = "TAIL"))
+    @Inject(
+        method = "addFixers",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/util/datafix/fixes/ItemStackCustomNameToOverrideComponentFix;<init>(Lcom/mojang/datafixers/schemas/Schema;)V"
+        ),
+        slice = @Slice(
+            from = @At(
+                value = "CONSTANT",
+                args = "intValue=3825"
+            ),
+            to = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/util/datafix/fixes/TrialSpawnerConfigFix;<init>(Lcom/mojang/datafixers/schemas/Schema;)V"
+            )
+        )
+    )
     private static void add3827Schema(DataFixerBuilder dataFixerBuilder, CallbackInfo ci) {
         Schema schema = dataFixerBuilder.addSchema(3827, SAME_NAMESPACED);
         dataFixerBuilder.addFixer(new RenameFilterableFix(schema));
